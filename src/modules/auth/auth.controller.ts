5bfe5ebc4105c719be648321a1a023529379e4a1
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2026-06-30 20:04:48 +0600

    refresh token api added

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -33,7 +33,29 @@ const loginUser = catchAsync(async (req : Request , res : Response , next : Next
     })
 })
 
+const refreshToken = catchAsync(async(req : Request , res : Response , next : NextFunction)  => {
+
+    const refreshToken = req.cookies.refreshToken;
+
+    const {accessToken} = await authService.refreshTokenFromDB(refreshToken)
+
+    res.cookie("accessToken" , accessToken , {
+        httpOnly : true,
+        secure : false,
+        sameSite : 'none',
+        maxAge : 1000 * 60 * 60 * 24 // 24 hour
+    })
+
+    sendResponse(res , {
+        success : true,
+        statusCode : httpStatus.OK,
+        message : "Token refreshed successfully!",
+        data : accessToken
+    })
+})
+
 
 export const authController = {
-    loginUser
+    loginUser,
+    refreshToken
 }

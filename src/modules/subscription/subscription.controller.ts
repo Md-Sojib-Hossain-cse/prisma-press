b9fde3500ba1bc14a3765bc83c1a5004ef7e2325
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2026-07-09 14:43:57 +0600

    stripe initialized and setup webhook

--- src/modules/subscription/subscription.controller.ts
+++ src/modules/subscription/subscription.controller.ts
@@ -1,8 +1,20 @@
 import { NextFunction, Request, Response } from "express"
 import { catchAsync } from "../../utils/catchAsync"
+import { subscriptionService } from "./subscription.service";
+import { sendResponse } from "../../utils/sendResponse";
+import httpStatus from "http-status"
 
 const createCheckoutSession = catchAsync( async (req : Request , res : Response , next : NextFunction) => {
-    
+    const userId = req.user?.id;
+
+    const result = await subscriptionService.createCheckoutSession(userId as string)
+
+    sendResponse(res , {
+        success : true,
+        statusCode : httpStatus.OK,
+        message : "Checkout completed successfully!",
+        data : result
+    })
 })
 
 export const subscriptionController = {
